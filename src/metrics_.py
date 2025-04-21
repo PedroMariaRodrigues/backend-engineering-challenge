7e931cf13d1fb17882f8e98521f6f8dd862b74ef
Pedro Rodrigues <pedro.maria.rodrigues@tecnico.ulisboa.pt>
2025-04-21 03:03:15 +0100

    Add compute methods for MovingAverage and Maximum metrics classes

--- src/metrics_.py
+++ src/metrics_.py
@@ -3,26 +3,49 @@ class Metrics:
     '''
     Class with the available metrics
     '''
+    def compute() -> int:
+        pass
+    
+    
+class MovingAverage(Metrics):
+    '''
+    Calculates the moving average of the events
+    '''
     #Note:
     # For cases with a huge amount of data:
     # Only need to calculate the first MA with all the data, 
     # after that it can be calculated adding the new data and removing data out of the time window 
     # and add the previous MA calculated
-           
-    def moving_average(events: list):
-        '''
-        Calculates the moving average        
-        '''
-        if events:
-            #calculate the moving average
-            delivery_time = []
-            for event in events:
-                delivery_time.append(event.duration)
-
-            ma = sum(delivery_time)/len(delivery_time)
-        else:
-            ma = 0
         
+    def compute(self, events: list) -> float:
+        # Check if events is empty
+        if not events:
+            return 0
+        
+        # Calculate the moving average
+        delivery_time = []
+        for event in events:
+            delivery_time.append(event.duration)
+        
+        ma = sum(delivery_time) / len(delivery_time)
         return ma
        
-    #Add other metrics
+#Add other metrics
+class Maximum(Metrics):
+    '''
+    Calculates the maximum of the events
+    '''
+    
+    def compute(self, events: list) -> float:
+        
+        # Check if events is empty
+        if not events:
+            return 0
+        
+        # Calculate the maximum
+        delivery_time = []
+        for event in events:
+            delivery_time.append(event.duration)
+        
+        max = max(delivery_time)
+        return max
